447281856b0bd8584afb45ad7b4e3ce3af94c622
yaustal666 <max.poz@mail.ru>
2022-11-14 19:17:21 +1000

    argh..

--- labs/third_lab.sql
+++ labs/third_lab.sql
@@ -1,12 +1,13 @@
 -- 1)Проанализировать созданную альтернативную таблицу Customer,
 -- вывести страны и количества записей для каждой из них (1 запросом)
 --|
-SELECT Country, COUNT(*) FROM CUSTOM
-GROUP BY Country;
+SELECT Country, COUNT(*) AS count FROM CUSTOM
+GROUP BY Country
+ORDER BY count DESC;
 --|
 -- 2)Для страны с самым большим количеством записей удалить из
 -- таблицы строку(ки) имеющую(ие) информацию о компании
---| некрасиво как-то, надо подумать ещё
+--|
 DELETE FROM CUSTOM
 WHERE Country = (SELECT Country FROM (SELECT Country, MAX(NUM)
                                       FROM (SELECT Country, COUNT(*) NUM FROM CUSTOM
@@ -15,10 +16,7 @@ AND Company IS NOT NULL;
 --|
 -- 3)Вставить данные из таблицы Employee в таблицу полученную в задании 2, отсутствующие данные заполнить NULL
 --|
-INSERT INTO CUSTOM(FirstName, LastName,
-                   Address, City, State,
-                   Country, PostalCode,
-                   Phone, Fax, Email)
+INSERT INTO CUSTOM(FirstName, LastName, Address, City, State, Country, PostalCode, Phone, Fax, Email)
 SELECT FirstName, LastName,
        Address, City, State,
        Country, PostalCode,
@@ -30,15 +28,13 @@ SELECT * FROM CUSTOM;
 -- 4)Добавить пользователя Evangeline Lilly
 --|
 INSERT INTO CUSTOM (FirstName, LastName)
-VALUES ("Evangeline", "Lilly");
+VALUES ('Evangeline', 'Lilly');
 --|
 -- 5)Исправить ошибку возникникшую в данных (подсказка задание 2 и 4)
 --|
-
+UPDATE CUSTOM
+SET CustomerId = ROWID
+WHERE CustomerId IS NULL;
 --|
 -- Упрощение: в пятом задании достаточно показать адекватную таблицу содержащую не все данные из предыдущей
-<<<<<<< HEAD
 -- а только 'FirstName','LastName' и исправленную колонку с ошибкой.
-=======
--- а только 'FirstName','LastName' и исправленную колонку с ошибкой."
->>>>>>> e7d4cc2ddab61057151a8a77ddd53df0f5eb0d8a
